31f06113dbaebfee82e30f2c6aab8c6247b2f846
vsevolodovna <sofyakrasik@gmail.com>
2024-11-22 22:51:04 +0000

    main CDR3 data

---
+++ BCR_CDR3_data/5RACE/CDR3AminoAcidProperties.sh
@@ -0,0 +1,4 @@
+
+##!/bin/bash
+#Calculate Averages Non-normalized
+vdjtools CalcCdrAaStats -r cdr3-full -a hydropathy,strength,charge,kf1,kf2,kf3,kf4,kf5,kf6,kf7,kf8,kf9,kf10 /home/sofyakrasik/BCR_CDR3_data/5RACE/VDJconverted/*IGH*txt /home/sofyakrasik/BCR_CDR3_data/5RACE/AminoAcidProperties/
